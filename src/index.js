f6ef80d3a23638980b7fd9c173a0f8916763c06f
esrmoe <esrmoeserhat@gmail.com>
2025-03-03 02:54:43 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CorridorMemoryGame from "./App";
+import "./styles.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CorridorMemoryGame />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
